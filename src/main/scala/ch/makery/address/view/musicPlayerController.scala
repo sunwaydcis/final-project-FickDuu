5bc43455f068dc8a471ac665b89fa95275bdd27c
Aaditya Joshua Srivastava <16014508@imail.sunway.edu.my>
2024-12-27 21:00:45 +0800

    making a music player app
    will supply 5 songs

---
+++ src/main/scala/ch/makery/address/view/musicPlayerController.scala
@@ -0,0 +1,35 @@
+package ch.makery.address.view
+
+import javafx.fxml.Initializable
+import java.util.ResourceBundle;
+
+public class musicPlayerController implements Initializable{
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1){
+
+  }
+
+  public void playMedia(){
+
+  }
+
+  public void pauseMedia() {
+
+  }
+
+  public void prevButton() {
+
+  }
+
+  public void nextButton() {
+
+  }
+
+  public void beginTimer() {
+
+  }
+
+  public void cancelTimer() {
+
+  }
+}
