4a35befa70612444642f4063b252acbbbd489e98
Aaditya Joshua Srivastava <16014508@imail.sunway.edu.my>
2024-12-27 18:00:03 +0800

    making smtg easier

--- src/main/scala/models/Card.scala
+++
@@ -1,7 +0,0 @@
-package models
-
-abstract class Card(val name: String, val description: String){
-  def cardType: String
-  def use(user: GameEntity, target: GameEntity): Unit
-  override def toString: String = s"$cardType: $name - $description"
-}
