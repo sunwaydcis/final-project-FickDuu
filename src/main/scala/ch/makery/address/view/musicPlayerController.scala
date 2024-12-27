427af860fd3e70657dced70a8b8db84bdbadef72
Aaditya Joshua Srivastava <16014508@imail.sunway.edu.my>
2024-12-28 05:04:51 +0800

    Update musicPlayerController.scala

--- src/main/scala/ch/makery/address/view/musicPlayerController.scala
+++ src/main/scala/ch/makery/address/view/musicPlayerController.scala
@@ -1,28 +1,28 @@
 package ch.makery.address.view
 
-import javafx.fxml.Initializable
 import scalafx.event.ActionEvent
-import scalafx.scene.control.{ComboBox, ProgressBar}
+import scalafx.scene.control.{Button, ComboBox, Label, ProgressBar, Slider}
 import scalafx.scene.layout.Pane
+import scalafx.beans.property.DoubleProperty
+import scalafx.beans.binding.Bindings
 import scalafx.scene.media.MediaPlayer
 
-import java.util.ResourceBundle
+import java.io.File
 import scala.collection.mutable.ArrayBuffer
-import scala.sys.process.processInternal.File;
 
 class musicPlayerController{
 
   @FXML private var body: Pane = _
-  @FXML private val songName: Label = null
-  @FXML private var playButton, pauseButton, resetButton, prevButton, nextButton: Button = null
-  @FXML private val speedBox: ComboBox[String] = null
-  @FXML private val volumeSlider: Slider = null
-  @FXML private val songProgBar: ProgressBar = null
+  @FXML private var songName: Label = _
+  @FXML private var playButton, pauseButton, resetButton, prevButton, nextButton: Button = _
+  @FXML private val speedBox: ComboBox[String] = _
+  @FXML private val volumeSlider: Slider = _
+  @FXML private val songProgBar: ProgressBar = _
 
-  private val songs = ArrayBuffer.[File]
+  private val songs = ArrayBuffer.empty[File]
   private var songNumber = 0
   private var mediaPlayer: MediaPlayer = _
-  private var media: Media = null
+  private var media: Media = _
 
   def initialize(): Unit = {
     val directory = new File("music")
@@ -35,175 +35,45 @@ class musicPlayerController{
     speedBox.items ++= Seq("25%", "50%", "75%", "100%", "125%", "150%", "175%", "200%")
     speedBox.onAction = _ => changeSpeed()
     volumeSlider.value <==> mediaPlayer.volume
+
+    songProgBar.progress <== Bindings.createDoubleBinding(
+      () => mediaPlayer.currentTime.toSeconds / media.duration.toSeconds,
+      mediaPlayer.currentTimeProperty()
+    )
   }
 
-  private Media media;
-  private MediaPlayer mediaPlayer;
-
-  private File directory;
-  private File[] files;
-
-  private ArrayList<File> songs;
-  private int songNumber;
-
-  private int[] speeds = {25, 50, 75, 100, 125, 150, 175, 200};
-  private Timer timer;
-  private TimerTask task;
-  private boolean running;
-
-  @Override
-  public void initialize(url arg0, ResourceBundle arg1){
-    songs = new ArrayList<File>();
-
-    directory = new File("music");
-
-    files = directory.listFiles();
-
-    if(files != null){
-      for(File file : files) {
-        songs.add(file);
-        System.out.println(file);
-      }
-    }
-
-    media = new Media(songs.get(songNumber).toURI().toString());
-    mediaPlayer = new MediaPlayer(media);
-
-    songName.setText(songs.get(songNumber).getName());
-
-    for(int i = 0; i < speeds.length; i++){
-      speedBox.getItems().add(Integer.toString(speeds[i]) + "%");
-    }
-
-    speedBox.setOnAction(this::changeSpeed);
-    volumeSlider.valueProperty().addListener(new ChangeListener<Number>(){
-
-      @Override
-      public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2){
-        mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
-      }
-    });
-
-    songProgBar.setStyle("-fx-accent: #f23d67;")
+  def playMedia(): Unit = {
+    mediaPlayer.play()
   }
 
-  public void playMedia(){
-
-    beginTimer();
-    changeSpeed(null);
-    mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
-    mediaPlayer.play();
+  def pauseMedia(): Unit = {
+    mediaPlayer.pause()
   }
 
-  public void pauseMedia() {
-    cancelTimer();
-    mediaPlayer.pause();
+  def resetMedia(): Unit = {
+    mediaPlayer.seek(Duration.ZERO)
   }
 
-  public void resetMedia(){
-
-    songProgBar.setProgress(0);
-    mediaPlayer.seek(Duration.seconds(0));
+  def prevMedia(): Unit = {
+    songNumber = (songNumber - 1 + songs.size) % songs.size
+    playCurrentSong()
   }
 
-  public void prevMedia() {
-    if (songNumber > 0) {
-      songNumber --;
-
-      mediaPlayer.stop();
-
-      if(running){
-        cancelTimer();
-      }
-
-      media = new Media(songs.get(songNumber).toURI().toString());
-      mediaPlayer = new MediaPlayer(media);
-
-      songName.setText(songs.get(songNumber).getName());
-
-      playMedia();
-    }
-    else {
-      songNumber = songs.size() - 1;
-
-      mediaPlayer.stop();
-      if (running) {
-        cancelTimer();
-      }
-
-      media = new Media(songs.get(songNumber).toURI().toString());
-      mediaPlayer = new MediaPlayer(media);
-
-      songName.setText(songs.get(songNumber).getName());
-
-      playMedia()
-    }
+  def nextMedia(): Unit = {
+    songNumber = (songNumber + 1) % songs.size
+    playCurrentSong()
   }
 
-  public void nextMedia() {
-    if(songNumber < songs.size() - 1){
-      songNumber++;
-
-      mediaPlayer.stop();
-      if (running) {
-        cancelTimer();
-      }
-
-      media = new Media(songs.get(songNumber).toURI().toString());
-      mediaPlayer = new MediaPlayer(media);
-
-      songName.setText(songs.get(songNumber).getName());
-
-      playMedia();
-    }
-    else{
-      songNumber = 0;
-
-      mediaPlayer.stop();
-      if (running) {
-        cancelTimer();
-      }
-
-      media = new Media(songs.get(songNumber).toURI().toString());
-      mediaPlayer = new MediaPlayer(media);
-
-      songName.setText(songs.get(songNumber).getName());
-
-      playMedia()
-    }
+  private def changeSpeed(): Unit = {
+    val speed = speedBox.value.value.dropRight(1).toInt / 100.0
+    mediaPlayer.setRate(speed)
   }
 
-  public void changeSpeed(ActionEvent event){
-    if(speedBox.getValue() == null){
-      mediaPlayer.setRate(1);
-    }
-    else{
-//      mediaPlayer.setRate(Integer.parseInt(speedBox.getValue()) * 0.01);
-      mediaPlayer.setRate(Integer.parseInt(speedBox.getValue().substring(beginIndex(0, speedBox.getValue().length() - 1))) * 0.01);
-    }
-  }
-
-  public void beginTimer(){
-    timer = new Timer();
-    task = new TimerTask(){
-        public void run(){
-          running = true;
-          double current = mediaPlayer.getCurrentTime().toSeconds();
-          double end = media.getDuration().toSeconds();
-          System.out.println(current/end);
-          songProgBar.setProgress(current/end);
-
-          if(current/end == 1){
-            cancelTimer();
-          }
-        }
-    };
-    timer.scheduleAtFixedRate(task, 0,1000);
-  }
-
-  public void cancelTimer() {
-    running = false;
-    timer.cancel();
-
+  private def playCurrentSong(): Unit = {
+    mediaPlayer.stop()
+    media = new Media(songs(songNumber).toURI.toString)
+    mediaPlayer = new MediaPlayer(media)
+    songName.text = songs(songNumber).getName
+    playMedia()
   }
 }
