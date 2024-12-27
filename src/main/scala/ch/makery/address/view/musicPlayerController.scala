597248e912ff0ff84774bc81f07ee218619e2866
Aaditya Joshua Srivastava <16014508@imail.sunway.edu.my>
2024-12-28 05:17:11 +0800

    Update musicPlayerController.scala

--- src/main/scala/ch/makery/address/view/musicPlayerController.scala
+++ src/main/scala/ch/makery/address/view/musicPlayerController.scala
@@ -1,13 +1,11 @@
 package ch.makery.address.view
 
-import scalafx.event.ActionEvent
-import scalafx.scene.control.{Button, ComboBox, Label, ProgressBar, Slider}
-import scalafx.scene.layout.Pane
-import scalafx.beans.property.DoubleProperty
-import scalafx.beans.binding.Bindings
-import scalafx.scene.media.MediaPlayer
-
-import java.io.File
+import javafx.fxml.FXML
+import javafx.scene.control.{Button, ComboBox, Label, ProgressBar, Slider}
+import javafx.scene.layout.Pane
+import javafx.scene.media.{Media, MediaPlayer}
+import javafx.collections.FXCollections
+import javafx.util.Duration
 import scala.collection.mutable.ArrayBuffer
 
 class musicPlayerController{
@@ -15,11 +13,11 @@ class musicPlayerController{
   @FXML private var body: Pane = _
   @FXML private var songName: Label = _
   @FXML private var playButton, pauseButton, resetButton, prevButton, nextButton: Button = _
-  @FXML private val speedBox: ComboBox[String] = _
-  @FXML private val volumeSlider: Slider = _
-  @FXML private val songProgBar: ProgressBar = _
+  @FXML private var speedBox: ComboBox[String] = _
+  @FXML private var volumeSlider: Slider = _
+  @FXML private var songProgBar: ProgressBar = _
 
-  private val songs = ArrayBuffer.empty[File]
+  private var songs = ArrayBuffer.empty[File]
   private var songNumber = 0
   private var mediaPlayer: MediaPlayer = _
   private var media: Media = _
@@ -32,7 +30,7 @@ class musicPlayerController{
     mediaPlayer = new MediaPlayer(media)
     songName.text = songs.head.getName
 
-    speedBox.items ++= Seq("25%", "50%", "75%", "100%", "125%", "150%", "175%", "200%")
+    speedBox.items = javafx.collections.FXCollections.observableArrayList("25%", "50%", "75%", "100%", "125%", "150%", "175%", "200%")
     speedBox.onAction = _ => changeSpeed()
     volumeSlider.value <==> mediaPlayer.volume
 
