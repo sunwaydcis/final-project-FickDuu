0935731552b244aaad80d903c4738baf6919fd20
Aaditya Joshua Srivastava <16014508@imail.sunway.edu.my>
2024-12-29 00:55:43 +0800

    adding features
    smooth song title scrolling
    progress bar on click control

--- src/main/scala/MusicPlayer/musicPlayerController.scala
+++ src/main/scala/MusicPlayer/musicPlayerController.scala
@@ -6,6 +6,9 @@ import javafx.scene.control.*
 import javafx.scene.layout.Pane
 import javafx.scene.media.{Media, MediaPlayer}
 import javafx.util.Duration
+import javafx.animation.Timeline
+import javafx.scene.text.Text
+import scalafx.animation.KeyFrame
 
 import java.io.File
 import scala.collection.mutable.ArrayBuffer
@@ -13,7 +16,7 @@ import scala.collection.mutable.ArrayBuffer
 class musicPlayerController {
 
   @FXML private var body: Pane = _
-  @FXML private var songName: Label = _
+  @FXML private var songName, timeRemaining: Label = _
   @FXML private var playButton, pauseButton, resetButton, prevButton, nextButton: Button = _
   @FXML private var speedBox: ComboBox[String] = _
   @FXML private var volumeSlider: Slider = _
@@ -23,26 +26,7 @@ class musicPlayerController {
   private var songNumber = 0
   private var mediaPlayer: MediaPlayer = _
   private var media: Media = _
-
-  def changeSpeed(): Unit = {
-    if (mediaPlayer != null && speedBox.getValue != null) {
-      val speed = speedBox.getValue.replace("%", "").toInt / 100.0
-      mediaPlayer.setRate(speed)
-    }
-  }
-
-  def playCurrentSong(): Unit = {
-    if (mediaPlayer != null) mediaPlayer.dispose()
-    if (songs.nonEmpty) {
-      media = new Media(songs(songNumber).toURI.toString)
-      mediaPlayer = new MediaPlayer(media)
-      songName.setText(songs(songNumber).getName)
-      bindMediaPlayerProperties()
-      playMedia()
-    } else{
-      songName.setText("No songs available")
-    }
-  }
+  private var scrollTimeline: Timeline = _
   
   def initialize(): Unit = {
     val musicDirURL = getClass.getResource("/music")
@@ -58,6 +42,7 @@ class musicPlayerController {
       media = new Media(songs.head.toURI.toString)
       mediaPlayer = new MediaPlayer(media)
       songName.setText(songs.head.getName)
+      scrollSongName()
     } else{
       songName.setText("No songs available")
     }
@@ -101,19 +86,88 @@ class musicPlayerController {
     }
   }
 
+  def seekMedia(eventX: Double): Unit = {
+    if(mediaPlayer != null && mediaPlayer.getTotalDuration != null){
+      val progress = eventX / songProgBar.getWidth
+      mediaPlayer.seek(mediaPlayer.getTotalDuration.multiply(progress))
+    }
+  }
+
   def bindMediaPlayerProperties(): Unit = {
-    if(mediaPlayer != null){
-      volumeSlider.valueProperty().addListener((_, _, newValue) =>{
+    if(mediaPlayer != null) {
+      volumeSlider.valueProperty().addListener((_, _, newValue) => {
         mediaPlayer.setVolume(newValue.doubleValue() / 100)
       })
       volumeSlider.setValue(mediaPlayer.getVolume * 100)
-    }
 
-    mediaPlayer.currentTimeProperty().addListener((_, _, newValue) =>{
-      val progress = if (mediaPlayer.getTotalDuration != null)
-        newValue.toSeconds / mediaPlayer.getTotalDuration.toSeconds
-      else 0.0
+      mediaPlayer.currentTimeProperty().addListener((_, _, newValue) => {
+        val progress = if (mediaPlayer.getTotalDuration != null)
+          newValue.toSeconds / mediaPlayer.getTotalDuration.toSeconds
+        else 0.0
         songProgBar.setProgress(progress)
-    })
+
+        val remainingTime = mediaPlayer.getTotalDuration.subtract(newValue)
+        val minutes = remainingTime.toMinutes.toInt
+        val seconds = (remainingTime.toSeconds % 60).toInt
+        timeRemaining.setText(f"$minutes%02d:$seconds%02d")
+      })
+
+      songProgBar.setOnMousePressed(event => seekMedia(event.getX))
+      songProgBar.setOnMouseDragged(event => seekMedia(event.getX))
+    }
+  }
+
+  def autoplayNextSong(): Unit = {
+    mediaPlayer.setOnEndOfMedia(() => nextMedia())
+  }
+
+  def changeSpeed(): Unit = {
+    if (mediaPlayer != null && speedBox.getValue != null) {
+      val speed = speedBox.getValue.replace("%", "").toInt / 100.0
+      mediaPlayer.setRate(speed)
+    }
+  }
+
+  def playCurrentSong(): Unit = {
+    if (mediaPlayer != null) mediaPlayer.dispose()
+    if (songs.nonEmpty) {
+      media = new Media(songs(songNumber).toURI.toString)
+      mediaPlayer = new MediaPlayer(media)
+      songName.setText(songs(songNumber).getName)
+      bindMediaPlayerProperties()
+      playMedia()
+      scrollSongName()
+      autoplayNextSong()
+    } else {
+      songName.setText("No songs available")
+    }
+  }
+
+  def scrollSongName(): Unit ={
+    if(scrollTimeline != null) scrollTimeline.stop()
+
+    val text = new Text(songName.getText)
+    text.setFont(songName.getFont)
+    val textWidth = text.getBoundsInLocal.getWidth
+    val labelWidth = songName.getWidth
+
+    if(textWidth > labelWidth){
+      val scrollDistance = textWidth - labelWidth
+      scrollTimeline = new Timeline(
+        new javafx.animation.KeyFrame(Duration.seconds(0), _ => {
+          if(songName != null) songName.setTranslateX(0)
+        }),
+        new javafx.animation.KeyFrame(Duration.seconds(5), _ => {
+          if(songName != null) songName.setTranslateX(-scrollDistance)
+        })
+      )
+      scrollTimeline.setCycleCount(javafx.animation.Animation.INDEFINITE)
+      scrollTimeline.setAutoReverse(true)
+      scrollTimeline.play()
+    } else{
+      songName.setTranslateX(0)
+    }
   }
 }
+
+
